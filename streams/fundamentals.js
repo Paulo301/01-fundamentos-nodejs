e88d3ac1c0ffe3dd3b7b70ada7d1bddfd05d8a0c
Paulo301 <paulovictor1918@gmail.com>
2023-08-13 12:58:59 -0300

    Stream de escrita e transformação

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,6 +1,6 @@
 // process.stdin.pipe(process.stdout)
 
-import { Readable } from "node:stream";
+import { Readable, Transform, Writable } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -13,11 +13,25 @@ class OneToHundredStream extends Readable {
         this.push(null);
       } else {
         const buf = Buffer.from(String(i));
-  
+
         this.push(buf);
       }
     }, 1000);
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream().pipe(new InverseNumberStream()).pipe(new MultiplyByTenStream());
