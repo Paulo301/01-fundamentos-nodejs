64597b9c686a7aade070552a5b7d0b5405e57cc5
Paulo301 <paulovictor1918@gmail.com>
2023-08-16 12:27:13 -0300

    Filtrando lista do banco de dados

--- src/database.js
+++ src/database.js
@@ -1,24 +1,34 @@
 import fs from "node:fs/promises";
 
-const databasePath = new URL('../db.json', import.meta.url);
+const databasePath = new URL("../db.json", import.meta.url);
 
 export class Database {
   #database = {};
 
   constructor() {
-    fs.readFile(databasePath, 'utf-8').then(data => {
-      this.#database = JSON.parse(data);
-    }).catch(() => {
-      this.#persist();
-    });
+    fs.readFile(databasePath, "utf-8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
   }
 
   #persist() {
     fs.writeFile(databasePath, JSON.stringify(this.#database));
   }
 
-  select(table) {
-    const data = this.#database[table] ?? [];
+  select(table, search) {
+    let data = this.#database[table] ?? [];
+
+    if (search) {
+      data = data.filter((row) => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
+    }
 
     return data;
   }
@@ -36,18 +46,18 @@ export class Database {
   }
 
   update(table, id, data) {
-    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
-    if(rowIndex > -1) {
+    if (rowIndex > -1) {
       this.#database[table][rowIndex] = { id, ...data };
       this.#persist();
     }
   }
 
   delete(table, id) {
-    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
-    if(rowIndex > -1) {
+    if (rowIndex > -1) {
       this.#database[table].splice(rowIndex, 1);
       this.#persist();
     }
